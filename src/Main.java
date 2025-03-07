2d1fc81d807250e154688209db601a7f8ed1642e
selinbsa <selinbasakagan@gmail.com>
2025-03-08 02:30:24 +0300

    Adding comments

--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,18 @@ public class Main {
 
         System.out.println("Lütfen boyunuzu (metre cinsinde) giriniz: " );
 
+        //Taking height from user
         double height = scanner.nextDouble();
 
         System.out.println("Lütfen kilonuzu giriniz: " );
 
+        //Taking weight from user
         double weight = scanner.nextDouble();
 
+        //Calculating result with inputs taking from user
         double calculate = weight / (height * height);
 
+        //Showing result to user
         System.out.println("Vücut Kitle Indeksiniz: " + calculate);
 
     }
